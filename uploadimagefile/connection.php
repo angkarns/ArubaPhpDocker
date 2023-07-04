8c5a422092db4d3f01bd0a153152ffb2e99646c3
angkarns <102656934+angkarns@users.noreply.github.com>
2023-07-04 14:45:35 +0700

    Delete connection.php

--- uploadimagefile/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("192.168.1.66", "root", "password", "iot");
- // Check Connection
- if (!$conn) {
-    die("Connection failed " . mysqli_connect_error());      // call "die" function
-} 
